acf4fcbc8ad355d19cb08d651ced21d9d3f8aa44
lanaafana <lanaafana@gmail.com>
2023-08-08 20:20:25 +0300

    Stage 1

---
+++ Traffic Light (Java)/task/src/traffic/Main.java
@@ -0,0 +1,12 @@
+package traffic;
+
+public class Main {
+  public static void main(String[] args){
+    System.out.println("Welcome to the traffic management system!\n" +
+            "Menu:\n" +
+            "1. Add\n" +
+            "2. Delete\n" +
+            "3. System\n" +
+            "0. Quit");
+  }
+}
